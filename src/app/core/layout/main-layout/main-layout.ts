e46afefd261b93dd00b27e3281a4bc796e2d5f96
Patricia <email>
2026-03-10 01:29:32 +0200

    Fixed toast integration

--- src/app/core/layout/main-layout/main-layout.ts
+++ src/app/core/layout/main-layout/main-layout.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 
 import { Header } from '../header/header';
 import { Footer } from '../footer/footer';
+import { ToastHost } from '../../../shared/components/toast-host/toast-host';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, ToastHost],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
